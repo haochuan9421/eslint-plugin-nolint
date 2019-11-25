b83cc429dd9842291524cb8c8764753466ce943a
HaoChuan9421 <hc199421@gmail.com>
2019-11-25 19:00:41 +0800

    feat: 依赖分离

--- config/vue.js
+++ config/vue.js
@@ -4,4 +4,17 @@ module.exports = {
     "plugin:vue/essential",
     "@vue/typescript",
   ],
+  rules: {
+    "no-param-reassign": [
+      "error",
+      {
+        props: true,
+        ignorePropertyModificationsFor: [
+          "state", // for vuex state
+          "acc", // for reduce accumulators
+          "e", // for e.returnvalue
+        ],
+      },
+    ],
+  },
 };
